48326b0e8e389d66d5daf3ab4633a199c3483cc4
Kabir Khan <kabirkhann1118@gmail.com>
2025-03-22 15:43:46 +0530

    setup React & tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl font-bold text-blue-200'>My App</h1>
+    </main>
+  )
+}
+
+export default App
